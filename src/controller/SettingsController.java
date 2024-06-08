da3a1c0768ba3879145a4a52900213be27bf76d3
Mark <sanamamarknoriel9@gmail.com>
2024-06-08 10:31:31 +0800

    changes

--- src/controller/SettingsController.java
+++ src/controller/SettingsController.java
@@ -200,7 +200,7 @@ public class SettingsController implements Initializable{
 
     @Override
     public void initialize(URL url, ResourceBundle resource){
-
+        System.out.println("hello World");
     }
     
 }
